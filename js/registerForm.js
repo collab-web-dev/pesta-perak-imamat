87335818722fd3c40f90a91e8890cae690629d43
collab-web-dev <kolabangela@gmail.com>
2026-09-20 23:18:38 +0700

    Update registerForm.js

--- js/registerForm.js
+++ js/registerForm.js
@@ -3,7 +3,7 @@ const form = document.getElementById('registrationForm');
 const confirmModal = document.getElementById('confirmModal');
 const successModal = document.getElementById('successModal');
 
-const phoneRegex = /^\+628[0-9]{8,11}$/;
+const phoneRegex = /^\08[0-9]{8,11}$/;
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 function toggleFormState(disabled) {
@@ -56,7 +56,7 @@ function validateForm() {
         showError(telephone, 'Wajib diisi');
         isValid = false;
     } else if (!phoneRegex.test(telephone.value.trim())) {
-        showError(telephone, 'Format nomor harus +628XXXXXXXXXX');
+        showError(telephone, 'Format nomor harus 08XXXXXXXXXX');
         isValid = false;
     } else {
         clearError(telephone);
